33ba11af25323b8950efa07c9ae921a1d380de15
Ben Parrish <82351654+sheepdoglord554@users.noreply.github.com>
2022-01-13 00:05:57 -0500

    Update and rename Day_Two.py to day_2.py

--- Advent of Code/Advent of Code 2021/Day Two/day_2.py
+++ Advent of Code/Advent of Code 2021/Day Two/day_2.py
@@ -29,47 +29,40 @@ After following these instructions, you would have a horizontal position of 15 a
 
 Calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
 """
-#import raw_movements as string from another python file in directory
-import movements
-raw_movements = movements.raw_movements
+#import .txt file of submarine movements and read to a single string variable
+opened_text = open(r"C:\Users\bentp\Desktop\Python\Advent of Code 2021\day_2.txt")
+read_text = opened_text.read()
 
-#convert raw movements string into list of raw "movements"
-raw_movements = raw_movement_string.split("\n")
+#convert our string of submarine movements into a list of strings
+movement_strings_raw = read_text.split("\n")
 
-#initialize a two entry python list that serves as a coordinate system denoting the x position first (forward and back), and the y position second (up and down)
+#initialize a list of two values to represent our current position. loop through the list of strings representing each movement, split the string into magnitude and direction, and adjust current_position variable using logic
 current_position = [0,0]
 
-#loop through the list of separated string movement entries and split into direction and magnitude denotation. then, use differentiated direction and magnitude to modify
-#the "current_position" variable piecewise for each entry in the "raw_movements" list
-for movement in raw_movements:
-  movement_tuple = movement.split(" ")
-  if movement_tuple[0] == "forward":
-    current_position[0]+=int(movement_tuple[1])
-  elif movement_tuple[0] == "up":
-    current_position[1]-=int(movement_tuple[1])
-  elif movement_tuple[0] == "down":
-    current_position[1]+=int(movement_tuple[1])
-print(current_position)
-
-#the solution to the problem demands the product of the position dimentions, which will be computed and printed below
-solution = current_position[0]*current_position[1]
-print(solution)
+for movement in movement_strings_raw:
+    movement_split = movement.split(" ")
+    direction = movement_split[0]
+    magnitude = movement_split[1]
+    if direction == "forward":
+        current_position[0]+=int(magnitude)
+    elif direction == "up":
+        current_position[1]-=int(magnitude)
+    elif direction == "down":
+        current_position[1]+=int(magnitude)
+ 
+print(current_position[0]*current_position[1])
 
 """
 --- Part Two ---
 Based on your calculations, the planned course doesn't seem to make any sense. You find the submarine manual and discover that the process is actually slightly more complicated.
-
 In addition to horizontal position and depth, you'll also need to track a third value, aim, which also starts at 0. The commands also mean something entirely different than you first thought:
-
 down X increases your aim by X units.
 up X decreases your aim by X units.
 forward X does two things:
 It increases your horizontal position by X units.
 It increases your depth by your aim multiplied by X.
 Again note that since you're on a submarine, down and up do the opposite of what you might expect: "down" means aiming in the positive direction.
-
 Now, the above example does something different:
-
 forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
 down 5 adds 5 to your aim, resulting in a value of 5.
 forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
@@ -77,26 +70,24 @@ up 3 decreases your aim by 3, resulting in a value of 2.
 down 8 adds 8 to your aim, resulting in a value of 10.
 forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
 After following these new instructions, you would have a horizontal position of 15 and a depth of 60. (Multiplying these produces 900.)
-
 Using this new interpretation of the commands, calculate the horizontal position and depth you would have after following the planned course. What do you get if you multiply your final horizontal position by your final depth?
-
 """
 
-#initialize a three entry python list that denotes dimensional values of the submarines current position. as above, index = 0 corresponds to the submarine's x position
-#index =1 corresponds to the submarine's y position, and index = 2 corresponds to the submarine's "aim" as described in the prompt
+#redefine current position variable with additional variable of 'aim'. alter previous algorithm by including aim variable in calculation of depth
 current_position = [0,0,0]
-#recreate the loop operation as above but accounting for "aim" with the third dimension denoted in the "current position" variable
-for movement in raw_movements:
-  movement_tuple = movement.split(" ")
-  if movement_tuple[0] == "forward":
-    current_position[0]+=int(movement_tuple[1])
-    current_position[1]+=(int(movement_tuple[1])*current_position[2])
-  elif movement_tuple[0] == "up":
-    current_position[2]-=int(movement_tuple[1])
-  elif movement_tuple[0] == "down":
-    current_position[2]+=int(movement_tuple[1])
-print(current_position)
 
-#the solution to the problem demands the product of the position dimentions, which will be computed and printed below
-solution = current_position[0]*current_position[1]
-print(solution)
+for movement in movement_strings_raw:
+    movement_split = movement.split(" ")
+    direction = movement_split[0]
+    magnitude = movement_split[1]
+    if direction == "up":
+        current_position[1]-=int(magnitude)
+        current_position[2]-=int(magnitude)
+    elif direction == "down":
+        current_position[1]+=int(magnitude)
+        current_position[2]+=int(magnitude)
+    elif direction == "forward":
+        current_position[0]+=int(magnitude)
+        current_position[1]+=(current_position[2]*int(magnitude))
+
+print(current_position[0]*current_position[1])
